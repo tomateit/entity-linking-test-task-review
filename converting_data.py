219b7d571c8179619b0f582053c4924e948b364f
Max Ermakov <maksim_ermakov@protonmail.com>
2023-07-02 18:04:48 +0000

    Added strict schema definition to input and output

--- converting_data.py
+++ converting_data.py
@@ -1,66 +1,128 @@
-import pandas as pd
 import json
+from pathlib import Path
+from dataclasses import dataclass, asdict
+
+import pandas as pd
 
 
-def read_jsonl_file(file_path):
+@dataclass(frozen=True)
+class InputSchema:
+    context: str
+    mention2entity: dict[str, list[str]]
+    entity2type: dict[str, list[str]]
+    id: int
+
+
+@dataclass(frozen=True)
+class EntitySchema:
+    qid: str
+
+
+@dataclass(frozen=True)
+class MentionSchema:
+    text_id: int
+    entity_id: str
+    span_start: int
+    span_end: int
+    span: str
+
+
+@dataclass(frozen=True)
+class TextSchema:
+    original_text: str
+    id: int
+
+
+def read_jsonl_file(file_path: Path) -> list[InputSchema]:
     data = []
-    with open(file_path, 'r') as file:
-        for line in file:
-            data.append(json.loads(line))
+    with open(file_path, "r") as file:
+        for idx, line in enumerate(file):
+            datum = json.loads(line)
+            datum = InputSchema(**datum, id=idx)
+            data.append(datum)
     return data
 
 
-def extract_data_from_jsonl(data):
-    texts_data = []
-    entities_data = []
-    mentions_data = []
+def process_entry(entry: InputSchema) -> tuple[set[EntitySchema], list[MentionSchema]]:
+    context = entry.context
+    mention2entity = entry.mention2entity
+
+    entities = set()
+    mentions = []
+
+    for mention, (entity_qid, *extra_entities) in mention2entity.items():
+        if len(extra_entities) > 0:
+            print(extra_entities)
+
+        entity = EntitySchema(qid=entity_qid)
+        entities.add(entity)
+
+        if mention not in context:
+            raise ValueErrord(f"Mention {mention} is not a substring of `{context}`")
+
+        span_start = context.index(mention)
+        span_end = span_start + len(mention)
+        span = mention
+
+        new_mention = MentionSchema(
+            text_id=entry.id,
+            entity_id=entity_qid,
+            span_start=span_start,
+            span_end=span_end,
+            span=span
+        )
+
+        mentions.append(new_mention)
+    
+    return entities, mentions
+
+
+def extract_entities_from_input_chunks(data: list[InputSchema]):
+    texts_buffer = []
+    entities_buffer = []
+    mentions_buffer = []
 
     for entry in data:
-        context = entry['context']
-        text_id = len(texts_data) + 1
-        texts_data.append({'original_text': context, 'id': text_id})
+        entities, mentions = process_entry(entry)
+        text = TextSchema(
+            original_text=entry.context,
+            id=entry.id
+        )
 
-        mention2entity = entry['mention2entity']
-        entity2type = entry['entity2type']
+        texts_buffer.append(text)
+        entities_buffer.extend(entities)
+        mentions_buffer.extend(mentions)
 
-        for mention, entities in mention2entity.items():
-            for entity in entities:
-                entity_id = entity
-                if mention in context:
-                    span_start = context.index(mention)
-                    span_end = span_start + len(mention)
-                    span = mention
-
-                    entities_data.append({'qid': entity_id})
-                    mentions_data.append({
-                        'text_id': text_id,
-                        'entity_id': entity_id,
-                        'span_start': span_start,
-                        'span_end': span_end,
-                        'span': span
-                    })
-
-    return texts_data, entities_data, mentions_data
-
-
-def save_dataframes_to_parquet(texts_data, entities_data, mentions_data, texts_file, entities_file, mentions_file):
-    texts_df = pd.DataFrame(texts_data)
-    entities_df = pd.DataFrame(entities_data)
-    mentions_df = pd.DataFrame(mentions_data)
-
-    texts_df.to_parquet(texts_file)
-    entities_df.to_parquet(entities_file)
-    mentions_df.to_parquet(mentions_file)
+    return texts_buffer, entities_buffer, mentions_buffer
 
 
 # Main function
 def main():
-    data = read_jsonl_file('test_seen_entities.jsonl')
+    # 1. Read in the data
+    data: list[InputSchema] = read_jsonl_file("test_seen_entities.jsonl")
+    
+    # 2. Transform and extract entities
     texts_data, entities_data, mentions_data = extract_data_from_jsonl(data)
-    save_dataframes_to_parquet(texts_data, entities_data, mentions_data,
-                               'test_seen_texts.parquet',
-                               'test_seen_entities.parquet',
-                               'test_seen_mentions.parquet')
+
+    # 3. Save texts as a dataframe
+    texts_path = Path("./test_seen_texts.parquet").resolve()
+    assert not texts_path.exists(), f"The output path contains a file already: {texts_path}"
+    texts_dataframe = pd.DataFrame([asdict(d) for d in texts_data])
+    texts_dataframe.to_parquet(texts_path)
+
+    # 4. Save entities as a dataframe
+    entities_path = Path("./test_seen_entities.parquet").resolve()
+    assert not entities_path.exists(), f"The output path contains a file already: {entities_path}"
+    entities_dataframe = pd.DataFrame([asdict(d) for d in entities_data])
+    entities_dataframe.to_parquet(entities_path)
+
+    # 5. Save mentions as a dataframe
+    mentions_path = Path("./test_seen_mentions.parquet").resolve()
+    assert not mentions_path.exists(), f"The output path contains a file already: {mentions_path}"
+    mentions_dataframe = pd.DataFrame([asdict(d) for d in mentions_data])
+    mentions_dataframe.to_parquet(mentions_path)
+
+    print("Done")
 
 
 if __name__ == '__main__':
