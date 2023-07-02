1b9c5077ff5cf80695c75388321bc53a919cf09a
Max Ermakov <maksim_ermakov@protonmail.com>
2023-07-02 18:05:02 +0000

    Adjusted tests to comply with updates schemas

--- test_converting_data.py
+++ test_converting_data.py
@@ -1,38 +1,49 @@
+import pytest
 import os
-from converting_data import extract_data_from_jsonl, save_dataframes_to_parquet
+from converting_data import (
+    InputSchema, EntitySchema, MentionSchema, TextSchema,
+    extract_entities_from_input_chunks
+)
 
-class TestDataProcessing:
-    def test_extract_data_from_jsonl(self):
-        data = [
-            {
-                "context": "An alternative approach to the comparison of Buddhist thought with Western philosophy is "
-                           "to use the concept of the Middle Way in Buddhism as a critical tool for the assessment of "
-                           "Western philosophies. In this way Western philosophies can be classified in Buddhist "
-                           "terms as eternalist or nihilist. In a Buddhist view all philosophies are considered "
-                           "non-essential views (ditthis) and not to be clung to.",
-                "mention2entity": {"Middle Way": ["Q833475"]},
-                "entity2type": {"Q833475": ["Q23847174"]}
-            }
-        ]
+text_blob = ("An alternative approach to the comparison of Buddhist thought with Western philosophy is "
+                "to use the concept of the Middle Way in Buddhism as a critical tool for the assessment of "
+                "Western philosophies. In this way Western philosophies can be classified in Buddhist "
+                "terms as eternalist or nihilist. In a Buddhist view all philosophies are considered "
+                "non-essential views (ditthis) and not to be clung to.")
 
-        texts_data, entities_data, mentions_data = extract_data_from_jsonl(data)
+test_input = InputSchema(
+    id=25635,
+    context=text_blob,
+    mention2entity={"Middle Way": ["Q833475"]},
+    entity2type={"Q833475": ["Q23847174"]},
+)
+
+test_output_mention = MentionSchema(
+    text_id=25635,
+    span="Middle Way",
+    span_start=115,
+    span_end=125,
+    entity_id="Q833475",
+)
+
+test_output_text = TextSchema(
+    original_text=text_blob,
+    id=25635,
+)
+
+test_output_entity = EntitySchema(
+    qid="Q833475"
+)
+
+
+class TestDatasetTransform:
+    def test_extract_data(self):
+        texts_data, entities_data, mentions_data = extract_entities_from_input_chunks([test_input])
 
         assert len(texts_data) == 1
         assert len(entities_data) == 1
         assert len(mentions_data) == 1
 
-    def test_save_dataframes_to_parquet(self):
-        texts_data = [{'original_text': 'UwU UwU UwU', 'id': 1}]
-        entities_data = [{'qid': '1'}, {'qid': '2'}]
-        mentions_data = [{'text_id': 1, 'entity_id': '1', 'span_start': 0, 'span_end': 5, 'span': 'Hello'},
-                         {'text_id': 1, 'entity_id': '2', 'span_start': 6, 'span_end': 11, 'span': 'world'}]
-
-        texts_file = 'unit_test_texts.parquet'
-        entities_file = 'unit_test_entities.parquet'
-        mentions_file = 'unit_test_mentions.parquet'
-
-        save_dataframes_to_parquet(texts_data, entities_data, mentions_data, texts_file, entities_file, mentions_file)
-
-        assert os.path.exists(texts_file)
-        assert os.path.exists(entities_file)
-        assert os.path.exists(mentions_file)
+        assert texts_data[0] == test_output_text
+        assert entities_data[0] == test_output_entity
+        assert mentions_data[0] == test_output_mention
